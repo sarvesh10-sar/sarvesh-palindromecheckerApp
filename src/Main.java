89fc515ce4d92fccc08f54f0d84f6e075153ae93
sarvesh10-sar <ss7303@srmist.edu.in>
2026-03-17 14:32:14 +0530

    UC5: Remove spaces for palindrome check

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,12 @@ public class Main {
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
 
-        String lower = str.toLowerCase();
-        String rev = new StringBuilder(lower).reverse().toString();
+        // UC5: Remove spaces + Case insensitive
+        String processed = str.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(processed).reverse().toString();
 
-        if(lower.equals(rev)) {
-            System.out.println("Palindrome (case-insensitive)");
+        if (processed.equals(reversed)) {
+            System.out.println("Palindrome (ignoring spaces & case)");
         } else {
             System.out.println("Not Palindrome");
         }
